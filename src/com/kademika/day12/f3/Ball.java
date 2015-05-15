22f4abea59c8eba0943bc66e4a8d3abd12fd7e82
Denys Bezprozvanyy <deanwhisper@gmail.com>
2015-05-15 19:36:19 +0300

    Test

--- src/com/kademika/day12/f3/Ball.java
+++ src/com/kademika/day12/f3/Ball.java
@@ -1,10 +1,87 @@
 package com.kademika.day12.f3;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by dean on 5/11/15.
  */
-public class Ball {
+public class Ball extends JComponent {
 
+    private int ballX;
+    private int ballY;
+    private int ballSpeed;
+    private Color ballColour;
 
+    public Ball(int ballY, int ballSpeed, Color ballColour) {
+        this.ballY = ballY;
+        this.ballSpeed = ballSpeed;
+        this.ballColour = ballColour;
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                move();
+//            }
+//        }.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(ballColour);
+        g.fillOval(ballX, ballY, 10, 10);
+    }
+
+    public void move() {
+        int direction = 1;
+        while (true) {
+            if (ballX == 600-10) {
+                direction = -1;
+                ballX--;
+            } else if (ballX == 0) {
+                direction = 1;
+                ballX++;
+            } else {
+                ballX = ballX+direction;
+            }
+            try {
+                Thread.sleep(ballSpeed);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public int getOvalX() {
+        return ballX;
+    }
+
+    public void setOvalX(int ovalX) {
+        this.ballX = ovalX;
+    }
+
+    public int getOvalY() {
+        return ballY;
+    }
+
+    public void setOvalY(int ovalY) {
+        this.ballY = ovalY;
+    }
+
+    public int getBallSpeed() {
+        return ballSpeed;
+    }
+
+    public void setBallSpeed(int ballSpeed) {
+        this.ballSpeed = ballSpeed;
+    }
+
+    public Color getBallColour() {
+        return ballColour;
+    }
+
+    public void setBallColour(Color ballColour) {
+        this.ballColour = ballColour;
+    }
 
 }
--- src/com/kademika/day12/f3/FlyingBall.java
+++ src/com/kademika/day12/f3/FlyingBall.java
@@ -11,70 +11,56 @@ public class FlyingBall extends JPanel {
     private long freq = 16;
     private int frameWidth=600;
     private int frameHeight=600;
-    private int ovalWidth=10;
-    private int ovalHeight=10;
-    private int ovalX;
-    private int ovalY;
-    private int ballSpeed;
-    private int ballColour;
-
+    private Ball ball1;
+    private Ball ball2;
+    private Ball ball3;
+    private Ball ball4;
+    private Ball ball5;
+    private Ball ball6;
+    private Ball ball7;
+    private Ball ball8;
+    private Ball ball9;
+    private Ball ball0;
 
     public static void main(String[] args) {
-        final FlyingBall ball = new FlyingBall();
 
-        ballFly(ball);
+
     }
 
     private static void ballFly(final FlyingBall ball) {
-        new Thread() {
-            @Override
-            public void run() {
-                ball.myRepaint(ball);
-            }
-        }.start();
-
-        new Thread() {
-            @Override
-            public void run() {
-                ball.move();
-            }
-        }.start();
+        final FlyingBall flyBall = new FlyingBall();
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                flyBall.myRepaint(flyBall);
+//            }
+//        }.start();
+        flyBall.myRepaint(flyBall);
     }
 
     public FlyingBall() {
+        ball1 = new Ball(10, 16, new Color(255,0,0));
+        ball2 = new Ball(25, 20, new Color(177, 255, 112));
+        ball3 = new Ball(40, 30, new Color(95, 136, 255));
+        ball4 = new Ball(55, 40, new Color(255, 90, 121));
+        ball5 = new Ball(70, 5, new Color(240, 255, 79));
+        ball6 = new Ball(95, 1, new Color(92, 255, 20));
+        ball7 = new Ball(110, 50, new Color(249, 255, 37));
+        ball8 = new Ball(135, 70, new Color(227, 209, 255));
+        ball9 = new Ball(150, 35, new Color(255, 65, 249));
+        ball0 = new Ball(175, 7, new Color(45, 255, 243));
         JFrame frame = new JFrame("Flying Ball v1.1");
         frame.setContentPane(this);
+        add(ball1); add(ball2); add(ball3); add(ball4); add(ball5);
+        add(ball6); add(ball7); add(ball8); add(ball9); add(ball0);
         frame.setBounds(200, 200, frameWidth, frameHeight);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(new Color(255, 0, 0));
-        g.fillOval(ovalX, 10, ovalWidth, ovalHeight);
-    }
-
     public void myRepaint(JPanel panel) {
         while (true) {
             panel.repaint();
-            mySleep(freq);
-        }
-    }
-
-    public void move() {
-        int direction = 1;
-        while (true) {
-            if (ovalX == frameWidth-10) {
-                direction = -1;
-                ovalX--;
-            } else if (ovalX == 0) {
-                direction = 1;
-                ovalX++;
-            } else {
-                ovalX = ovalX+direction;
-            }
             mySleep(freq);
         }
     }
@@ -87,35 +73,4 @@ public class FlyingBall extends JPanel {
         }
     }
 
-    public int getOvalX() {
-        return ovalX;
-    }
-
-    public void setOvalX(int ovalX) {
-        this.ovalX = ovalX;
-    }
-
-    public int getOvalY() {
-        return ovalY;
-    }
-
-    public void setOvalY(int ovalY) {
-        this.ovalY = ovalY;
-    }
-
-    public int getBallSpeed() {
-        return ballSpeed;
-    }
-
-    public void setBallSpeed(int ballSpeed) {
-        this.ballSpeed = ballSpeed;
-    }
-
-    public int getBallColour() {
-        return ballColour;
-    }
-
-    public void setBallColour(int ballColour) {
-        this.ballColour = ballColour;
-    }
 }
